3655c16d17eb4721da9433731bdc276c209318ab
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-09-14 22:32:44 +0200

    Write preprocessor script for primary data.

---
+++ scripts/preprocess_data.sh
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+
+import csv
+import os
+from pathlib import Path
+
+def is_core(row: dict):
+    return "core" in row['device_name'] and "core." not in row['device_name']
+
+def is_package(row: dict):
+    return "package" in row['device_name']
+
+def read_csv(path: Path):
+    with open(path, "r", encoding="utf-8") as data_file:
+        reader = csv.reader(data_file, delimiter=',')
+        dict_reader = csv.DictReader(data_file, fieldnames=next(reader))
+        return list(dict_reader)
+
+def filter_data(rows: list[dict]):
+    return [
+        r for r in rows
+        if is_package(r) or is_core(r)
+    ]
+
+def get_plugin_type(row: dict):
+    return "RAPL" if "CPU" in row['device_name'] else "RYZEN"
+
+def process_data(rows: list[dict]):
+    return [
+        {
+            'plugin': get_plugin_type(r),
+            'region': r['region_idf'],
+            'energy_uj': int(r['energy']),
+            'time_us': int(r['time'])
+        }
+        for r in rows
+    ]
+
+def write_csv(path: Path, data: list[dict]):
+    with open(
+            Path(f"./processed/{path.name}.processed.csv"),
+            "w", encoding="utf-8"
+        ) as f:
+        header = [k for k in data[0].keys()]
+        writer = csv.DictWriter(f, fieldnames=header)
+        writer.writeheader()
+        writer.writerows(data)
+
+def main():
+    if not "run" in Path(os.getcwd()).name:
+        raise Exception('Script must be run in a test run data directory!')
+
+    if not len([
+        p for p in os.listdir('.') if p != 'logs' and p != 'processed'
+    ]):
+        raise Exception('No data to process!')
+
+    for rp in os.listdir('.'):
+        if rp != "logs" and rp != "processed":
+            path = Path(rp)
+            data = read_csv(path)
+            filtered = filter_data(data)
+            processed = process_data(filtered)
+            write_csv(path, processed)
+
+if __name__ == "__main__":
+    main()
